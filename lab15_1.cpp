ffe5e4778fa78c23464e7b3ce8c3089932b2f139
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-01-11 17:45:43 +0700

    Lab15_1Edit

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i, j;
+ 	T key;
+    for (i = 1; i < N; i++)
+    { 
+        key = d[i]; 
+        j = i - 1; 
 
-}
+        while (j >= 0 && d[j] < key)
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = key; 
+		cout << "pass " << i << ":";
+		int i ;
+		for (i = 0; i < N; i++) 
+       	cout << d[i] << " "; 
+    	cout << endl;
+    }
+} 
+
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
